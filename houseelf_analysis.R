c5e13e40e1a25de134b4eb18a4eaf8b4d5cae8ae
Grad480 <Grad480>
2016-10-12 17:23:58 -0500

    Created a function to calculate the gc content of a dna sequence.

--- houseelf_analysis.R
+++ houseelf_analysis.R
@@ -1,2 +1,11 @@
 #Script for analyzing houseelf earlength dna data
-helf_dat<-read.csv("./data/houseelf_dna_data.csv",header=TRUE)
+library(stringr)
+helf_dat<-read.csv("./data/houseelf_dna_data_1.csv",header=TRUE)
+helf_gc<-function(sequence){
+  g<-str_count(str_to_lower(helf_dat$dnaseq),pattern="g")
+  c<-str_count(str_to_lower(helf_dat$dnaseq),pattern="c")
+  dna<-str_length(helf_dat$dnaseq)
+  gc<-((g+c)/dna)
+  return(gc)
+}
+helf_gc(helf_dat)
